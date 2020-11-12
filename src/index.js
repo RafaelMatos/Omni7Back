93dddbe6287d3d8d38485c27b2b4c43c3a145540
rafaelibiapina <rafaelibiapina@sefaz.pi.gov.br>
2020-11-12 13:31:53 -0300

    Estrutura backend OmniWeek7

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const cors = require('cors')
+// const { nextTick } = require('process')
+
+const app = express()
+
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+mongoose.connect('mongodb+srv://teste:teste@cluster0.mp4en.mongodb.net/week7?retryWrites=true&w=majority',{
+    useNewUrlParser : true,
+     useUnifiedTopology: true ,
+})
+
+app.use((req,res,next)=>{
+    req.io = io
+    next()
+})
+
+app.use(cors())
+
+app.use('/files',express.static(path.resolve(__dirname,'..','uploads','resized')))
+
+app.use(require('./routes'))
+
+server.listen(3333)
